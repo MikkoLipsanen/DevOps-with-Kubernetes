b297b0721d92fc7c8ba75eaa9c371dbcd24b45b0
Lipsanen Mikko (KA) <mikko.lipsanen@kansallisarkisto.fi>
2025-05-05 23:02:46 +0300

    Exercise 1.05

--- Part_1/Project/app.js
+++ Part_1/Project/app.js
@@ -1,12 +1,14 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
-const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello Kube!')
-});
+  res.sendFile(path.join(__dirname, '/index.html'))
+})
 
+// Start the server
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
-  console.log(`Server started in port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`)
 })
