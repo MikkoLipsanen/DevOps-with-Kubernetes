84be8563488e6ee58f06d07aad6af9a531bd25f6
Lipsanen Mikko (KA) <mikko.lipsanen@kansallisarkisto.fi>
2025-05-29 15:00:46 +0300

    Exercise 2.07

--- Pingpong/app.js
+++ Pingpong/app.js
@@ -1,17 +1,69 @@
 const express = require('express')
-const path = require('path')
-const fs = require('node:fs')
+const { Client } = require("pg");
+require('dotenv').config();
 const app = express()
 
-var counter = 0
+process.on('uncaughtException', function (err) {
+  console.log(err);
+});
 
-app.get('/pingpong', (req, res) => {
-    res.send(`Pongs: ${counter.toString()}`)
-    counter += 1
+const table = process.env.TABLE;
+const col = process.env.COLUMN;
+
+const getClient = async () => {
+  const client = new Client({
+    password: process.env.POSTGRES_PASSWORD,
+    user: process.env.POSTGRES_USER,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.POSTGRES_DB,
+  });
+  await client.connect();
+  return client;
+};
+
+const insertValue = async(client, value) => {
+  const text = `INSERT INTO ${table}(${col}) VALUES ($1)`
+  await client.query(text, [value])
+};
+
+const createTable = async (client) => {
+  const createTableText = `
+    CREATE TABLE IF NOT EXISTS ${table} (
+      id SERIAL PRIMARY KEY,
+      ${col} SMALLINT DEFAULT 0
+    );`
+  await client.query(createTableText)
+  await insertValue(client, 0)
+
+};
+
+const getCounter = async(client) => {
+  await createTable(client)
+  const text = `SELECT ${col} FROM ${table} WHERE id = 1`
+  const result = await client.query(text)
+  return result.rows[0]
+};
+
+const updateCounter = async (client, value) => {
+  const text = `UPDATE ${table} SET ${col} = $1 WHERE id = $2`
+  const values = [value, 1];
+  const res = await client.query(text, values)
+};
+
+app.get('/pingpong', async(req, res) => {
+  const client = await getClient();
+  const counter = await getCounter(client);
+  res.send(`Pongs: ${counter.count}`)
+  await updateCounter(client, counter.count + 1)
+  client.end();
 })
 
-app.get('/', (req, res) => {
-    res.send(counter.toString())
+app.get('/', async(req, res) => {
+  const client = await getClient();
+  const counter = await getCounter(client);
+  res.send(counter.count)
+  client.end();
 })
 
 // Start the server
